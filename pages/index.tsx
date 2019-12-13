428c1842cb1d8c0f5f70f523fc5e1a8b2aa59c41
Albert Arvesu <albertarvesu@gmail.com>
2019-12-13 18:35:20 +1100

    GA-Tracking

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+import ReactGA from 'react-ga';
 import Head from 'next/head';
 import {
   LogoWrapper,
@@ -16,26 +17,35 @@ import { SITE_NAME, SITE_TITLE } from '../constants';
 import './index.css';
 import newsProviders from './../providers.json';
 
-export default () => (
-  <React.Fragment>
-    <Head>
-      <title>{`${SITE_NAME} - ${SITE_TITLE}`}</title>
-    </Head>
-    <PageWrapper>
-      <NavWrapper>
-        <LogoWrapper>
-          <img height={50} width={50} src="/static/juan.png" />
-          <NavTitle>{SITE_NAME}</NavTitle>
-        </LogoWrapper>
-        <NavText>{SITE_TITLE}</NavText>
-      </NavWrapper>
-      <Container topOuterSpacing={2}>
-        <StyledGrid>
-          {newsProviders.map(item => (
-            <Block key={item.name} {...item} />
-          ))}
-        </StyledGrid>
-      </Container>
-    </PageWrapper>
-  </React.Fragment>
-);
+const Index: React.FC = () => {
+  useEffect(() => {
+    ReactGA.initialize(process.env.GOOGLE_ANALYTICS_CODE || 'G-D1W540KSYE');
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+  }, []);
+  return (
+    <React.Fragment>
+      <Head>
+        <title>{`${SITE_NAME} - ${SITE_TITLE}`}</title>
+      </Head>
+      <PageWrapper>
+        <NavWrapper>
+          <LogoWrapper>
+            <img height={50} width={50} src="/static/juan.png" />
+            <NavTitle>{SITE_NAME}</NavTitle>
+          </LogoWrapper>
+          <NavText>{SITE_TITLE}</NavText>
+        </NavWrapper>
+        <Container topOuterSpacing={2}>
+          <StyledGrid>
+            {newsProviders.map(item => (
+              <Block key={item.name} {...item} />
+            ))}
+          </StyledGrid>
+        </Container>
+      </PageWrapper>
+    </React.Fragment>
+  );
+};
+
+export default Index;
